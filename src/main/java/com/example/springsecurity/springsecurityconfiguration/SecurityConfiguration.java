069ce3296c12bcefca949a755245e4cba0eb8e25
ajaylamoria <39006824+ajaylamoria@users.noreply.github.com>
2021-01-17 23:31:00 +0530

    Added JPA CRUD Operations

--- src/main/java/com/example/springsecurity/springsecurityconfiguration/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/springsecurityconfiguration/SecurityConfiguration.java
@@ -3,20 +3,16 @@ package com.example.springsecurity.springsecurityconfiguration;
 
 import javax.sql.DataSource;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @EnableWebSecurity
 @Configuration
--- src/main/java/com/example/springsecurity/springsecurityconfiguration/controller/HomeResource.java
+++ src/main/java/com/example/springsecurity/springsecurityconfiguration/controller/HomeResource.java
@@ -1,4 +1,4 @@
-package com.example.springsecurity.controller;
+package com.example.springsecurity.springsecurityconfiguration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
